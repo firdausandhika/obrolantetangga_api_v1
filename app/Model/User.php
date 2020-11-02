1b7893791d45318950c21acbd8d5b0386d3deeec
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2020-11-02 14:20:25 +0800

    === PERSIAPAN AWAL  ======

--- app/Model/User.php
+++ app/Model/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $guarded = [];
     protected $appends = ['is_new'];
-    protected $hidden = ['otp'];
+    // protected $hidden = [''];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -29,7 +29,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','otp',
     ];
 
     /**
