299033c9b531e614c060406afba746c1a38b1e8b
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-04-25 13:53:51 +0800

    cek iklan baris

--- app/Http/Controllers/V1/IklanBarisController.php
+++ app/Http/Controllers/V1/IklanBarisController.php
@@ -103,7 +103,7 @@ class IklanBarisController extends V1Controller
               'user_id'=>$this->user->id,
               'wilayah'=>$this->user->kelurahan,
               'unik'=> Str::random(20),
-              'number_wa'=>  ? $request->no_wa : null,
+              'number_wa'=>  $is_send_wa ? $request->no_wa : null,
               'kontent'=>\nl2br(htmlspecialchars($request->kontent)),
             ]);
           } catch (\Exception $e) {
