3bc1664c81f87507da1bc26908d09c52a22d12de
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-19 14:54:41 +0800

    -------

--- app/Model/User.php
+++ app/Model/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $guarded = [];
-    // protected $appends = ['is_new','avatar_url','cover_url','nama_propinsi','nama_kota','nama_kecamatan','nama_kelurahan'];
+    protected $appends = ['is_new','avatar_url','cover_url','nama_propinsi','nama_kota','nama_kecamatan','nama_kelurahan'];
     // protected $hidden = [''];
 
     /**
