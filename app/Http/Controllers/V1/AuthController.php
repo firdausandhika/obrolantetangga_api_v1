bc984e4b16a720d8732d13585f413d29496178a4
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-23 01:05:18 +0800

    debug tetangga

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -146,7 +146,7 @@ class AuthController extends V1Controller
 
     public function send_sms_link_reset($password_reset,$users)
     {
-        print_r("masuk sini");exit;
+        print_r([env('KEY_WA_DEMO'),env('URL_WA'),env('AUTH_WA_KEY')]);exit;
         $number = $users->nomor;
         $rtrim = $number - rtrim('0');
         // $users = User::wherePhone($rtrim)->first();
