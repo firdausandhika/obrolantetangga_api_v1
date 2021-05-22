8dda586b36558ece3a5598bbbd7175f6334ac929
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-22 14:40:24 +0800

    debug tetangga

--- app/Http/Controllers/V1/TetanggaController.php
+++ app/Http/Controllers/V1/TetanggaController.php
@@ -52,7 +52,7 @@ class TetanggaController extends V1Controller
       $n_tetangga = $query->count();
 
       $this->res->msg   = "Success";
-      $this->res->data   = ["tetanggas"=>$tetanggas,"jumlah_tetangga"=>$n_tetangga];
+      $this->res->data   = ["tetanggas"=>"1","jumlah_tetangga"=>"1"];
       return \response()->json($this->res);
     }
 
