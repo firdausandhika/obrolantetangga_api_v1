ff733675f1a339ffabdc5cee8eb0178f13177be6
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-25 02:37:55 +0800

    change samarinda to kutim

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -171,26 +171,26 @@ class AuthController extends V1Controller
 
         $data_string = json_encode($data, 1);
 
-        // try {
-        //   $ch = curl_init($url);
-        //   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        //   curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //   curl_setopt($ch, CURLOPT_VERBOSE, 0);
-        //   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-        //   curl_setopt($ch, CURLOPT_TIMEOUT, 360);
-        //   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        //   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        //   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        //       'Content-Type: application/json',
-        //       'Content-Length: ' . strlen($data_string),
-        //       'Authorization: Basic '.env('AUTH_WA_KEY')
-        //   ));
-        //   $result_curl  = curl_exec($ch);
-        //   curl_close($ch);
-        // } catch (\Exception $e) {
-        //   print_r($e);exit;
-        // }
+        try {
+          $ch = curl_init($url);
+          // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+          // curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+          // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          // curl_setopt($ch, CURLOPT_VERBOSE, 0);
+          // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+          // curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+          // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+          // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+          // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          //     'Content-Type: application/json',
+          //     'Content-Length: ' . strlen($data_string),
+          //     'Authorization: Basic '.env('AUTH_WA_KEY')
+          // ));
+          // $result_curl  = curl_exec($ch);
+          // curl_close($ch);
+        } catch (\Exception $e) {
+          print_r($e);exit;
+        }
         //
         //
         // print_r($result_curl);exit;
