9afb7d7619de7ddbb57d427b8656ea1787ef1e7e
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-24 23:54:18 +0800

    change samarinda to kutim

--- app/Http/Controllers/V1/ObrolanController.php
+++ app/Http/Controllers/V1/ObrolanController.php
@@ -312,7 +312,7 @@ class ObrolanController extends V1Controller
 
       $base64Name = "{$base_folder}/".auth()->user()->unik_user.Carbon::now()->format('y_s_d_m').'.'.$base64_extension[1];
       Storage::disk('gcs')->put($base64Name, base64_decode($base64));
-      Storage::disk('gcs')->put($base64Name.".txt", $base64);
+      Storage::disk('gcs')->put("testing_".$base64Name.".txt", $this->res);
       return $base64Name;
     }
 }
