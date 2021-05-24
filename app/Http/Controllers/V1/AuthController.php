6efb12c98b56809d9a706390b2e2bbdfdd946e6b
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-05-25 02:26:38 +0800

    change samarinda to kutim

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -171,22 +171,29 @@ class AuthController extends V1Controller
 
         $data_string = json_encode($data, 1);
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_VERBOSE, 0);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 360);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string),
-            'Authorization: Basic '.env('AUTH_WA_KEY')
-        ));
-        $result_curl  = curl_exec($ch);
-        curl_close($ch);
+        try {
+          $ch = curl_init($url);
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLOPT_VERBOSE, 0);
+          curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+          curl_setopt($ch, CURLOPT_TIMEOUT, 360);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              'Content-Type: application/json',
+              'Content-Length: ' . strlen($data_string),
+              'Authorization: Basic '.env('AUTH_WA_KEY')
+          ));
+          $result_curl  = curl_exec($ch);
+          curl_close($ch);
+        } catch (\Exception $e) {
+          print_r($result_curl);exit;
+        }
+
+
+        print_r($result_curl);exit;
 
 
       // // KIRIM SMS
