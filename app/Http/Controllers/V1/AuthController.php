bd80eed59821d152c3a0c5de9ebcf887ff7f0ff7
firdausandhika <80946102+firdausandhika@users.noreply.github.com>
2021-05-25 15:05:31 +0700

    Update AuthController.php

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -171,7 +171,7 @@ class AuthController extends V1Controller
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
           curl_setopt($ch, CURLOPT_VERBOSE, 0);
           curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-          // curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+          curl_setopt($ch, CURLOPT_TIMEOUT, 100s);
           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
           curl_setopt($ch, CURLOPT_HTTPHEADER, array(
