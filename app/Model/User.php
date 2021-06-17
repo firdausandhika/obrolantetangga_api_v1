14c93fbc1f5fc8a3ea659129ea2958eb20507c1e
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-06-17 13:40:26 +0800

    add user admin

--- app/Model/User.php
+++ app/Model/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token','otp','deleted_at','updated_at','kategori_id','user_id','phone'
+        'password', 'remember_token','otp','deleted_at','kategori_id','user_id','phone'
     ];
 
     // protected $hidden = [];
