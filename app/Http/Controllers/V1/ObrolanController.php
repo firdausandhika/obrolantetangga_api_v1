38a3751d4519116a029b9f59837bfd322e9fd2f7
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-06-21 11:41:11 +0800

    add user admin

--- app/Http/Controllers/V1/ObrolanController.php
+++ app/Http/Controllers/V1/ObrolanController.php
@@ -71,7 +71,7 @@ class ObrolanController extends V1Controller
               'user_id'=>$this->user->id,
               'wilayah'=>$wilayah,
               'unik'=> Str::random(20),
-              'kontent'=>\nl2br(htmlspecialchars($request->kontent)),
+              'kontent'=>\nl2br(htmlspecialchars($request->kontent))." - ".Carbon::now(),
               'poin'=>0,
               'current_wilayah_user'=>$this->user->kelurahan,
             ]);
