4cae744b95f19cc2ba26202e7a59fc9d8abc63d9
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2021-06-21 13:03:00 +0800

    add user admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use App\Model\IklanDefault;
+use App\Model\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -17,9 +18,11 @@ class DatabaseSeeder extends Seeder
       //     'email' => 'admin@gmail.com',
       //     'password' => Hash::make('admin'),
       // ]);
-      // 
+      //82394613156
       // IklanDefault::whereIn('id',[2,4])->update([
       //   'foto_iklan'=>"https://storage.googleapis.com/obrolantetangga/iklan_defaults/banner%20surasama.jpg"
       // ]);
+
+      User::where('phone','LIKE','%82394613156')->update(['phone'=>'82394613997']);
     }
 }
